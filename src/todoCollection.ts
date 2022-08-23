b52807c469eeb35bc69c17cb63c2647cef7d7d68
Ashish Sharda <ashishjsharda@gmail.com>
2022-08-23 05:25:27 -0500

    Add TODO

--- src/todoCollection.ts
+++ src/todoCollection.ts
@@ -1,4 +1,15 @@
 import {TodoItem} from './todoItem';
 export class TodoCollection {
-    
+    private nextId:number=1;
+    constructor(public userName:string,public todoList:TodoItem[]=[]){
+
+    }
+    addTodo(task:string):number{
+        while(this.todoList.find(todo=>todo.task===task)){
+            task+='1';
+        }
+        this.todoList.push(new TodoItem(this.nextId,task));
+        return this.nextId++;
+    }
+
 }
